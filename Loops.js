bac2fd5b50404bff177519113f102b4fbfcd6c2c
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-08-07 19:30:23 -0700

    August 7, 2021

---
+++ Loops.js
@@ -0,0 +1,27 @@
+// The 'while' loop
+
+let i = 0;
+while (i < 3) { // shows 0, then 1, then 2
+  console.log( i );
+  i++;
+}
+
+
+let o = 5;
+while (o) {
+    console.log(o);
+    o--;
+}
+
+
+let m = 10;
+while (m) console.log(m--);
+
+
+//the 'do... while' loop
+
+let x = 0;
+do {
+  console.log(x);
+  x++;
+} while (x < 7);
