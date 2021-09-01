25ab0b311a8f8f7e3ab22c462a4a6015f9d1ef18
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-09-01 14:46:36 -0700

    Sept 1, 2021

--- TheOdinProject/06_leapYears/leapYears.js
+++
@@ -1,9 +0,0 @@
-//Leap years are years divisible by four (like 1984 and 2004). However,
-//years divisible by 100 are not leap years (such as 1800 and 1900) unless
-//they are divisible by 400 (like 1600 and 2000, which were in fact leap years).
-
-const leapYears = function(year) {
-    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0)
-  };
-  
-module.exports = leapYears;
